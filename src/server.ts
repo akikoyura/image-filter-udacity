571f66f8c455052c448dbe1722a2dcf161957424
Darak\vinsmoke <HoangMH1>
2022-08-23 22:10:30 +0700

    push code into github

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+    // Init the Express application
+    const app = express();
+
+    // Set the network port
+    const port = process.env.PORT || 8082;
+
+    // Use the body parser middleware for post requests
+    app.use(bodyParser.json());
+
+    app.get('/filteredimage', async (req, res) => {
+        const img_url = req.query;
+        if (!img_url) {
+            return res.status(400).send({message: "img_url is required. please provide valid url"});
+        } else {
+            filterImageFromURL(img_url).then(path => {
+                res.sendFile(path, error => {
+                    deleteLocalFiles([path]);
+                })
+            }).catch(error => {
+                res.status(400).send({message: 'img_url is required. please provide valid url'})
+            });
+        }
+    });
+
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get("/", async (req, res) => {
+        res.send("try GET /filteredimage?image_url={{}}")
+    });
+
+
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+})();
